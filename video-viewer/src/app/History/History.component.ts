c08ffdd7a75ab99df2d71be51c515e41dc512bd5
ania stryjewska <ania.stryjewska@gmail.com>
2021-03-25 22:28:55 +0100

    appended a test list of videos

--- video-viewer/src/app/History/History.component.ts
+++ video-viewer/src/app/History/History.component.ts
@@ -5,4 +5,16 @@ import {Component} from "@angular/core";
     templateUrl: './History.component.html'
 })
 
-export class HistoryComponent {}
+export class HistoryComponent {
+   videosHistory = [
+   {
+       "id":1,
+       "title": "first video"
+    },
+    {
+        "id":2,
+        "title": "second video"
+     }
+   ]
+
+}
